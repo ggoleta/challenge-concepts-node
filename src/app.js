9372d141a550c3f07c0edef928f82a5b184d194b
ggoleta <rogerio_contato@hotmail.com>
2020-04-09 23:21:54 -0300

    added middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -9,6 +9,16 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+
+function validateId(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid Repository ID.'})
+  }
+
+  return next();
+}
 
 app.get("/repositories", (request, response) => {
   // TODO
@@ -28,7 +38,7 @@ app.post("/repositories", (request, response) => {
   return response.json( repository );
 });
 
-app.put("/repositories/:id", (request, response) => {
+app.put("/repositories/:id", validateId, (request, response) => {
   const { id } = request.params;
   const { title, url, techs } = request.body;
   const repositorieIndex = repositories.findIndex( repo => repo.id === id);
@@ -53,7 +63,7 @@ app.put("/repositories/:id", (request, response) => {
 
 });
 
-app.delete("/repositories/:id", (request, response) => {
+app.delete("/repositories/:id",validateId, (request, response) => {
   const { id } = request.params;
   const repositorieIndex = repositories.findIndex( repo => repo.id === id);
   
